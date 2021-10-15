6fb1f9442f22701398d9bf2293eb96dc9a34f08f
Gonzalo Balabasquer <gbalabasquer@gmail.com>
2021-10-15 09:50:18 -0300

    Change gas to real values for Arbitrum

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ export SETH_ASYNC=yes
 
 for i in {0..233}
 do
-    ETH_NONCE=$i ETH_GAS=21000 seth send "$ETH_FROM"
+    ETH_NONCE=$i ETH_GAS=740000 seth send "$ETH_FROM"
 done
 
-ETH_NONCE=234 ETH_GAS=2000000 dapp create ProxyFactory
+ETH_NONCE=234 ETH_GAS=20000000 dapp create ProxyFactory
